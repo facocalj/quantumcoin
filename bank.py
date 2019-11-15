941ec2a9b1890b67e1773e9678c0506fe5a6e288
Hazel <hazmurray93@gmail.com>
2019-11-15 17:23:19 +0000

    Adding updated files and removing old ones

--- bank.py
+++ bank.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from random import randint, random, sample
+import random
+from random import randint, sample
 from time import sleep
+import math
 from cqc.pythonLib import CQCConnection, qubit
 
 # Default coin size
@@ -12,14 +14,14 @@ coins = []
 # Global qubit pair container
 pairs = []
 
+k_index = 9
 P = []
 
-def create_coin(k=2):
+def create_coin(k):
     print("First stage")
     with CQCConnection("Alice") as Bank:
         # For each i-th coin
         for i in range(k):
-            print("Creating the random number sequence")
             # Create a new empty coin
             coins.append([None] * coin_size)
             # Create a new qubit container
@@ -28,12 +30,12 @@ def create_coin(k=2):
             P.append(i)
 
             # Iterate over the newly created coin
+            print("Creating the classical random number sequence")
             for j in range(coin_size):
                 # Create random bit
                 random_bit = randint(0,1)
                 # Assign random bit to the coin container
                 coins[i][j] = random_bit
-
             print(coins[i])
 
             print('Creating qubits')
@@ -41,38 +43,16 @@ def create_coin(k=2):
             q1 = qubit(Bank)
             q2 = qubit(Bank)
 
-            print('Modulating qubits:')
-            # Encode coins into qubits
             for index, q in enumerate([q1, q2]):
                 offset = int(2 * index)
-                if coins[i][0+offset:2+offset] == [0,0]:
-                    # |0>
-                    print('Qubit', index+1, 'Encode: |0>' )
-
-                elif coins[i][0+offset:2+offset] == [0,1]:
-                    q.X()
-                    print('Qubit', index+1, 'Encode: |1>')
-
-                elif coins[i][0+offset:2+offset] == [1,0]:
-                    # |1>
-                    q.X()
-                    # |0>-|1>/2
-                    q.H()
-                    print('Qubit', index+1, 'Encode: (|0>-|1>)/2')
-
-                elif coins[i][0+offset:2+offset] == [1,1]:
-                    # |0>+|1>/2
-                    q.H()
-                    print('Qubit', index+1, 'Encode: (|0>+|1>)/2')
-
-                else:
-                    print('Opsie', coins[i])
-
+		#See section 2 of pdf. Run through gates based on classical input. 
+                q.H()
+                print('Qubit', index+1, 'via Hardamand')
+            
             # Store the encoded qubits
             pairs[i] = [q1, q2]
-
-            print('Finished coin:', i)
-
+            print('Finished quantum register:', i)
+            
             # Send the identifier
             Bank.sendClassical("Bob", P[i])
             # Send first half of the two qubits
@@ -86,11 +66,53 @@ def create_coin(k=2):
         # FLush memory
         Bank.flush()
 
-
 def parse_coin():
     pass
 
+def verify_coin(register):
+    # Hardcoding t at this point - to be randomised later
+    register_c = list(register)
+    print('Full register')
+    print(register_c)
+    t=3*(randint(1,math.floor(len(register_c)/3)))
+    print('t=',t)
+    m_s = []
+    list_of_random_indexes = random.sample(register_c, t)
+    print('Bank chosen list of t indexes')
+    print(list_of_random_indexes)
+    with CQCConnection("Alice") as Bank:
+        # Send the list of indexes
+        Bank.sendClassical("Bob", list_of_random_indexes)
+        # Wait for receiver
+        sleep(1)
+        index_list = Bank.recvClassical()
+        rlist = list(index_list)
+        print('Holder, Bob, chosen list of 2t/3 indexes')
+        print(rlist)
+        ## Wait for receiver
+        sleep(1)
+        print('Full register again')
+        print(register_c)
+	#foreach index in Bob's list, generate m= 0 or 1. 
+	#mark index as used.
+        for c,i in enumerate(rlist):
+            register_c.remove(i)
+            m_s.append(random.randint(0,1))
+        print('Remaining unused registers')
+        print(register_c)
+        print('m= 0 or 1 for each index chosen by Bob')
+        print(m_s)
+        Bank.sendClassical("Bob", m_s)
+
+    #FLush memory
+    #Bank.flush()
+
+
 if __name__ == "__main__":
-    create_coin()
+    create_coin(k_index)
+    
+    verify_coin(range(k_index))
 
     parse_coin()
+
+
--- bob.py
+++ bob.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from random import randint, random, sample
+import random
+#from random import randint, random, sample
 from time import sleep
 from cqc.pythonLib import CQCConnection, qubit
 
@@ -9,8 +10,8 @@ P = []
 # Global qubit pair container
 pairs = []
 
-def receive_coin(k=2):
-    print("First stage: Receive coins")
+def receive_coin(k=9):
+    print("First stage: Receive quantum registers for coin")
     with CQCConnection("Bob") as Bob:
         # For each i-th coin
         for i in range(k):
@@ -27,10 +28,48 @@ def receive_coin(k=2):
 
         sleep(1)
 
+def verify_coin(k=9):
+    print("Verify Coin ID 1")
+    with CQCConnection("Bob") as Bob:
+        register_c = list(range(k))#note hardcoded.
+        print('Full register')
+        print(register_c)
+        list_of_random_indexes = list(Bob.recvClassical())
+        print('Bank chosen list of t indexes')
+        print(list_of_random_indexes)
+        size = len(list_of_random_indexes)
+        smaller_t=int(2*size/3)
+        print('2t/3 = ',smaller_t)
+        local_selection = random.sample(list_of_random_indexes, smaller_t)
+        print('Holder, Bob, chosen list of 2t/3 indexes')
+        print(local_selection)
+        Bob.sendClassical("Alice", local_selection)
+        print('Remaining unused registers')
+        for c,i in enumerate(local_selection):
+            register_c.remove(i)
+
+        print(register_c)
+        # Wait for receiver
+        #sleep(1)
+        m_s = Bob.recvClassical()
+        print('Recieve m= 0 or 1 for each 2t/3 index chosen')
+        print(list(m_s))
+
+        for i in range(len(local_selection)):
+            print("Index value", local_selection[i], ", M Value", m_s[i])
+
+
+    # FLush memory
+    #Bob.flush()
+
+
 def send_coins():
     pass
 
 if __name__ == "__main__":
     receive_coin()
 
+    verify_coin()
+
     send_coins()
+
